01688d6ede16b2a4fb8e3ba2484ff641ed1dc6b0
ganeshphatangare-rw <79680237+ganeshphatangare-rw@users.noreply.github.com>
2021-06-22 16:51:07 +0530

    Updated the COS bucket url
    Updated the COS bucket URL with release v7.4.0l.332 and IBM discovery tool v4

--- rmm-template.tf
+++ rmm-template.tf
@@ -4,7 +4,7 @@ variable "TF_VERSION" {
 }
 
 variable "image_url" {
-  default = "cos://us-east/rackware-rmm-bucket/RackwareGoldenTemplate.qcow2"
+  default = "cos://us-east/rackware-rmm-bucket/RackWareRMMTemplate.qcow2"
 }
 
 /**
