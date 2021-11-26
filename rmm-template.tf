a5ed435dd55c85fa93fd8144730bc635df72241f
ganeshphatangare-rw <79680237+ganeshphatangare-rw@users.noreply.github.com>
2021-11-26 18:23:44 +0530

    Updated the COS bucket URL
    Updated the COS bucket URL to reflect latest GA release on IBM Paid tile v7.4.0.458

--- rmm-template.tf
+++ rmm-template.tf
@@ -4,7 +4,7 @@ variable "TF_VERSION" {
 }
 
 variable "image_url" {
-  default = "cos://us-east/rackware-rmm-bucket/RackWareRMMTemplate.qcow2"
+  default = "cos://us-east/rackware-rmm-bucket/RackWareIBMPaidNov2021.qcow2"
 }
 
 /**
